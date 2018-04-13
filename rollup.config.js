a90bd28ac554c1326d1f50894a6ddde4cfee4dd8
bencefr <bence.frenyo@nordicsemi.no>
2018-04-13 07:58:37 +0200

    added json plugin to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,26 @@
 import buble from 'rollup-plugin-buble';
 import async from 'rollup-plugin-async';
+import json from 'rollup-plugin-json';
 import pkg from './package.json';
 
 export default [
     {
-        input: pkg.module,
+        input: 'src/index.js',
         output: [
             { file: pkg.main, format: 'cjs', sourcemap: true },
         ],
         external: ['events', 'debug', 'usb', 'serialport', 'pc-nrfjprog-js', 'nrf-device-lister'],
         plugins: [
+            json({
+                include: 'src/**/*.json',
+                preferConst: true,
+                indent: '  ',
+            }),
             async(),
-            buble({ transforms: { generator: false } }),
+            buble({
+                exclude: ['src/**/*.json'],
+                transforms: { generator: false },
+            }),
         ],
     },
 ];
